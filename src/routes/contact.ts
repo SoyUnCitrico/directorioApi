aee55cd5741af56d5d59220e84e7ae7f12280dab
SoyUnCitrico <1darenastolteca@gmail.com>
2023-06-11 04:44:22 -0600

    Cambios en los permisos de los contactos!

--- src/routes/contact.ts
+++ src/routes/contact.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
+import { checkJwt } from '../middleware/session';
 import { getContactos, postContacto, getContacto,updateContacto, deleteContacto } from "../controllers/contact";
 import { logMiddleware } from "../middleware/log";
 
 const router = Router();
 
-router.get("/", logMiddleware, getContactos)
-router.get("/:id", getContacto)
-router.post("/", postContacto);
-router.put("/:id", updateContacto)
-router.delete('/:id', deleteContacto)
+router.get("/", getContactos)
+router.get("/:id", checkJwt, getContacto)
+router.post("/",  checkJwt, postContacto);
+router.put("/:id",  checkJwt, updateContacto)
+router.delete('/:id',  checkJwt, deleteContacto)
 
-export {router }
+export {router}
