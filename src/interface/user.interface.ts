beaa858eecf41cb7c9d136cc78a178700c964c9f
SoyUnCitrico <1darenastolteca@gmail.com>
2023-06-11 05:15:35 -0600

    Cambios en el interface

--- src/interface/user.interface.ts
+++ src/interface/user.interface.ts
@@ -2,6 +2,6 @@ import { Contact } from "./contact.interface";
 import { Auth } from "./auth.interface";
 
 export interface User extends Auth {
-    directorio: Array<Contact>
+    directorio: Array<string>
     isAdmin: boolean
 }
